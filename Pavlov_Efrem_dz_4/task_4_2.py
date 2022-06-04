b12889ff2707fc9e83f1ad3a8b4dad008775141a
Efrem Pavlov <contact@efrempavlov.ru>
2022-06-04 12:00:53 +0300

    Some updates in task_4_2.py

--- Pavlov_Efrem_dz_4/task_4_2.py
+++ Pavlov_Efrem_dz_4/task_4_2.py
@@ -1,15 +1,15 @@
 def currency_rates(code):
     from requests import get
+    from decimal import Decimal
     response = str(get('http://www.cbr.ru/scripts/XML_daily.asp').content).split('</Valute>')
     keys, values = [], []
     for data in response:
         data = data.split('<')
-        for key in data:
-            if key.startswith('CharCode>'):
-                keys.append(key[9:])
-        for value in data:
-            if value.startswith('Value>'):
-                values.append(float(value[6:].replace(',', '.')))
+        for obj in data:
+            if obj.startswith('CharCode>'):
+                keys.append(obj[9:])
+            elif obj.startswith('Value>'):
+                values.append(Decimal(obj[6:].replace(',', '.')))
     currency = dict(zip(keys, values))
     return currency.get(code.upper())
 
