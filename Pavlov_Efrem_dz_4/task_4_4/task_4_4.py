a1956d109d8829572290e41dfe186cbbb4454db3
Efrem Pavlov <contact@efrempavlov.ru>
2022-06-06 11:07:58 +0300

    Fixes in task 4

--- Pavlov_Efrem_dz_4/task_4_4/task_4_4.py
+++ Pavlov_Efrem_dz_4/task_4_4/task_4_4.py
@@ -1,4 +1,7 @@
 import utils
 
+
+# Тестирую работу функции
 print(*utils.currency_rates('USD'))
 print(*utils.currency_rates('EUR'))
+print(utils.currency_rates('jfdjihfdhkhd'))
--- Pavlov_Efrem_dz_4/task_4_4/utils.py
+++ Pavlov_Efrem_dz_4/task_4_4/utils.py
@@ -1,6 +1,5 @@
 def currency_rates(code):
     from requests import get
-    from decimal import Decimal
     import datetime
     response = str(get('http://www.cbr.ru/scripts/XML_daily.asp').content).split('</Valute>')
     keys, values = [], []
@@ -10,14 +9,14 @@ def currency_rates(code):
             if obj.startswith('CharCode>'):
                 keys.append(obj[9:])
             elif obj.startswith('Value>'):
-                values.append(Decimal(obj[6:].replace(',', '.')))
+                values.append(float(obj[6:].replace(',', '.')))
             elif obj.startswith('ValCurs Date="'):
                 date = obj[14:24]
                 dd, mm, yy = map(int, date.split('.'))
                 date = datetime.date(yy, mm, dd)
     currency = dict(zip(keys, values))
     if currency.get(code.upper()):
-        return [currency.get(code.upper()), date]
+        return currency.get(code.upper()), date
 
 
 # Создал лишние команды, которые не должны выполниться при импорте
